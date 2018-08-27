25068d8122f309b2b03bc60c3fabde4fe1b178bd
Matej Mazur <matej.mazur@mail.com>
2018-08-27 12:53:02 +0200

    Update ObjectFieldTemplate.test.js

--- tests/ObjectFieldTemplate.test.js
+++ tests/ObjectFieldTemplate.test.js
@@ -1,20 +1,9 @@
 import React, { PureComponent } from "react";
 
-import { expect } from "chai";
-import { createFormComponent, createSandbox } from "./test_utils";
+import { createFormComponent } from "./test-utils";
 
 describe("ObjectFieldTemplate", () => {
-  let sandbox;
-
   const formData = { foo: "bar", bar: "foo" };
-
-  beforeEach(() => {
-    sandbox = createSandbox();
-  });
-
-  afterEach(() => {
-    sandbox.restore();
-  });
 
   class ObjectFieldTemplate extends PureComponent {
     render() {
@@ -57,18 +46,18 @@ describe("ObjectFieldTemplate", () => {
   });
 
   it("should render one root element", () => {
-    expect(node.querySelectorAll(".root")).to.have.length.of(1);
+    expect(node.querySelectorAll(".root")).toHaveLength(1);
   });
 
   it("should render one title", () => {
-    expect(node.querySelectorAll(".title-template")).to.have.length.of(1);
+    expect(node.querySelectorAll(".title-template")).toHaveLength(1);
   });
 
   it("should render one description", () => {
-    expect(node.querySelectorAll(".description-template")).to.have.length.of(1);
+    expect(node.querySelectorAll(".description-template")).toHaveLength(1);
   });
 
   it("should render two property containers", () => {
-    expect(node.querySelectorAll(".property")).to.have.length.of(2);
+    expect(node.querySelectorAll(".property")).toHaveLength(2);
   });
 });
