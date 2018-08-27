d94acd012336a7a33ef4de7c2db19e23e9b9cf29
Matej Mazur <matej.mazur@mail.com>
2018-08-27 12:10:04 +0200

    Update TitleTemplate.test.js

--- tests/TitleTemplate.test.js
+++ tests/TitleTemplate.test.js
@@ -1,26 +1,12 @@
 import React from "react";
-import { expect } from "chai";
 
 import TitleTemplate from "../src/components/templates/TitleTemplate";
-import { createSandbox, createComponent } from "./test_utils";
+import { createComponent } from "./test-utils";
 
 describe("TitleTemplate", () => {
-  let sandbox;
-
-  beforeEach(() => {
-    sandbox = createSandbox();
-  });
-
-  afterEach(() => {
-    sandbox.restore();
-  });
-
   // For some reason, stateless components needs to be wrapped into a stateful
   // one to be rendered into the document.
   class TitleTemplateWrapper extends React.Component {
-    constructor(props) {
-      super(props);
-    }
     render() {
       return <TitleTemplate {...this.props} />;
     }
@@ -29,41 +15,41 @@ describe("TitleTemplate", () => {
   it("should return a legend", () => {
     const props = {
       title: "Field title",
-      required: true,
+      required: true
     };
     const { node } = createComponent(TitleTemplateWrapper, props);
 
-    expect(node.tagName).to.equal("LEGEND");
+    expect(node.tagName).toBe("LEGEND");
   });
 
   it("should have the expected id", () => {
     const props = {
       title: "Field title",
       required: true,
-      id: "sample_id",
+      id: "sample_id"
     };
     const { node } = createComponent(TitleTemplateWrapper, props);
 
-    expect(node.id).to.equal("sample_id");
+    expect(node.id).toBe("sample_id");
   });
 
   it("should include only title, when field is not required", () => {
     const props = {
       title: "Field title",
-      required: false,
+      required: false
     };
     const { node } = createComponent(TitleTemplateWrapper, props);
 
-    expect(node.textContent).to.equal(props.title);
+    expect(node.textContent).toBe(props.title);
   });
 
   it("should add an asterisk to the title, when field is required", () => {
     const props = {
       title: "Field title",
-      required: true,
+      required: true
     };
     const { node } = createComponent(TitleTemplateWrapper, props);
 
-    expect(node.textContent).to.equal(props.title + "*");
+    expect(node.textContent).toBe(props.title + "*");
   });
 });
